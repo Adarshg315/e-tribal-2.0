1aaca688747ed55637e3035be93e7dbebf302e51
Adarshg315 <adarshg315@gmail.com>
2021-03-27 13:33:06 +0530

    added snipcart

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import products from "../products.json";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://app.snipcart.com" />
+				<link rel="preconnect" href="https://cdn.snipcart.com" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css"
+				/>
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Welcome to <a href="">E-tribal</a>
+				</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+				<p className={styles.description}>
+					<a
+						className="snipcart-checkout snipcart-summary"
+						href="#"
+						style={{ textDecoration: "none" }}
+					>
+						<strong>Cart:</strong>{" "}
+						<span className="snipcart-total-price">$0.00</span>
+					</a>
+				</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+				{/* <p className={styles.description}>
+					Get started by editing{" "}
+					<code className={styles.code}>pages/index.js</code>
+				</p> */}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+				<div className={styles.grid}>
+					{products.map((product) => {
+						return (
+							<div key={product.id} className={styles.card}>
+								<img src={product.image} alt={`Preview of ${product.title}`} />
+								<h3>{product.title}</h3>
+								<p>{product.description}</p>
+								<p>${product.price}</p>
+								<p>
+									<button
+										className="snipcart-add-item"
+										data-item-id={product.id}
+										data-item-image={product.image}
+										data-item-name={product.title}
+										data-item-url="/"
+										data-item-price={product.price}
+									>
+										Add to Cart
+									</button>
+								</p>
+							</div>
+						);
+					})}
+				</div>
+			</main>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			<footer className={styles.footer}>
+				<a
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Powered by{" "}
+					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+				</a>
+			</footer>
+			<script
+				async
+				src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js"
+			/>
+			<div
+				hidden
+				id="snipcart"
+				data-api-key="YzU1NjQ5ZjYtMDliNi00ODQxLWIxYTYtOTNmNmVhNDU1NTFmNjM3MTYyNDgwNTYzNjM1NDA1"
+			/>
+		</div>
+	);
 }
